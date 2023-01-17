11555d16e9f57fe3000a1e9e5fa462259ce1e9ea
DongHyunKim <akainoo123@gmail.com>
2023-01-17 14:49:24 +0900

    `feat` : eslint & prettier 규칙 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,13 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'unused-imports'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -17,9 +20,58 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'import/order': [
+      1,
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+    'import/newline-after-import': [1],
+    'import/no-useless-path-segments': [
+      'error',
+      {
+        noUselessIndex: true,
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    // 같은 역할을 하는 2개의 규칙에서 하나는 끄고, 다른 하나에 세분화된 규칙을 명시
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }, // _ 를 사용하는 경우 no-unused-vars 에 걸리지 않음
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'warn',
+    'import/no-unresolved': ['error', { ignore: ['^@', '^glob:'] }],
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-console': [
+      1,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'no-empty-pattern': 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['export'],
+      },
+      {
+        blankLine: 'any',
+        prev: ['export'],
+        next: ['export'],
+      },
+    ],
   },
 };
