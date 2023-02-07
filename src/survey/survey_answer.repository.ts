e5e48eb19a399a2fca913b4e7c94db004585e7ba
PH5555 <lmkn5342@gmail.com>
2023-02-07 14:41:06 +0900

    refactor: apply foreach, const variable

--- src/survey/survey_answer.repository.ts
+++ src/survey/survey_answer.repository.ts
@@ -6,18 +6,19 @@ import { Survey, SurveyAnswer } from '../entities';
 export class SurveyAnswerRepository extends Repository<SurveyAnswer> {
   async getSurveyAnswers(survey: Survey): Promise<SurveyAnswer[]> {
     const answers = [];
-    for (const question of survey['surveyQuestions']) {
-      answers.push(
-        await this.findOne({
-          relations: ['question', 'answerType', 'singleChoiceAnswers'],
-          where: {
-            question: {
-              questionId: question.questionId,
-            },
-          },
-        }),
-      );
-    }
+
+    survey['surveyQuestions'].forEach(async (question) => {
+      const { questionId } = question;
+      const questionRelation = ['question', 'answerType', 'singleChoiceAnswers'];
+      const questionQuery = {
+        question: { questionId },
+      };
+      const answer = await this.findOne({
+        relations: questionRelation,
+        where: questionQuery,
+      });
+      answers.push(answer);
+    });
     return answers;
   }
 }
